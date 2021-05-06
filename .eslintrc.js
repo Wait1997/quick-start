be462a4220009fdcf512656f677f7dcc3a12d20c
xiongguofang <xiongguofang@qq.com>
2021-05-06 12:06:03 +0800

    check eslint & prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
+/**
+ * "off"或0- 关闭规则
+ * "warn"或1- 开启规则，使用警告级别的错误：warn(不会导致程序退出)
+ * "error"或2- 开启规则，使用错误级别的错误：error(当被触发的时候，程序会退出)
+ */
+
 const OFF = 0
 const WARN = 1
 const ERROR = 2
@@ -8,6 +14,18 @@ module.exports = {
     es2021: true,
     node: true
   },
+  /**
+   * extends属性值可以由以下组成：
+   * plugin:
+   * 包名 (省略了前缀，比如，react)
+   * /
+   * 配置名称 (比如recommended)
+   * ---------------------------------------
+   * 插件一个主要的作用就是补充规则
+   * 比如eslint:recommended中没有有关react的规则
+   * 则需要另外导入规则插件eslint-plugin-react
+   * ---------------------------------------
+   */
   extends: [
     'airbnb',
     'airbnb/hooks',
@@ -15,26 +33,27 @@ module.exports = {
     'plugin:unicorn/recommended',
     'plugin:promise/recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'prettier/react',
-    'prettier/unicorn'
+    'plugin:prettier/recommended'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
+      impliedStrict: true,
       jsx: true
     },
-    ecmaVersion: 12,
+    ecmaVersion: 12, // 支持的es的版本 12表示2021年
     sourceType: 'module'
   },
+  // plugins属性值可以省略包名的前缀eslint-plugin-
+  plugins: ['react', 'unicorn', 'promise', '@typescript-eslint', 'prettier'],
   settings: {
     'import/resolver': {
       node: {
         extensions: ['.tsx', '.ts', '.js', '.json']
-      }
+      },
+      typescript: {}
     }
   },
-  plugins: ['react', 'unicorn', 'promise', '@typescript-eslint'],
   rules: {
     'import/extensions': [
       ERROR,
@@ -80,10 +99,12 @@ module.exports = {
     'unicorn/prefer-query-selector': ERROR,
     'unicorn/no-null': OFF,
     'unicorn/no-array-reduce': OFF,
+    'unicorn/import-style': OFF,
+    'unicorn/prefer-module': OFF,
 
     '@typescript-eslint/no-useless-constructor': ERROR,
     '@typescript-eslint/no-empty-function': WARN,
-    '@typescript-eslint/no-var-requires': OFF,
+    '@typescript-eslint/no-var-requires': OFF, // 关闭 require() 引入报错
     '@typescript-eslint/explicit-function-return-type': OFF,
     '@typescript-eslint/explicit-module-boundary-types': OFF,
     '@typescript-eslint/no-explicit-any': OFF,
@@ -104,18 +125,19 @@ module.exports = {
     'jsx-a11y/no-noninteractive-element-interactions': OFF,
     'jsx-a11y/no-static-element-interactions': OFF,
 
-    'lines-between-class-members': [ERROR, 'always'],
-    'linebreak-style': [ERROR, 'unix'],
-    quotes: [ERROR, 'single'],
-    semi: [ERROR, 'always'],
+    'lines-between-class-members': [ERROR, 'always'], // 要求或禁止类成员之间出现空行
+    'linebreak-style': [ERROR, 'unix'], // 强制使用一致的换行风格
+    quotes: [ERROR, 'single'], // 强制使用单引号
+    semi: [ERROR, 'never'],
     'no-unused-expressions': WARN,
     'no-plusplus': OFF,
-    'no-console': OFF,
+    'no-console': WARN,
     'class-methods-use-this': ERROR,
     'jsx-quotes': [ERROR, 'prefer-single'],
-    'global-require': OFF,
-    'no-use-before-define': OFF,
-    'no-restricted-syntax': OFF,
+    'global-require': OFF, // 要求 require() 出现在顶层模块作用域中
+    'no-use-before-define': WARN, // 禁止在变量定义之前使用它们
+    'no-restricted-syntax': OFF, // 禁用特定的语法
+    'no-var': WARN,
     'no-continue': OFF
   }
 }
--- README.md
+++ README.md
@@ -20,6 +20,7 @@
    # semi 末尾是否添加分号
    # Object、Array最后是否添加逗号
    # singleQuote 是否使用单引号
+   # printWidth 但行字符最长的长度
    {
      "semi": true,
      "trailingComma": "none",
@@ -39,3 +40,99 @@ plugins
 - elsint
 - @typescript-eslint/parser
 - @typescript-eslint/eslint-plugin
+
+1. `eslint-config-airbnb` 使用
+   package:
+   - eslint-config-airbnb
+   - eslint
+   - eslint-plugin-import
+   - eslint-plugin-react
+   - eslint-plugin-react-hooks
+   - eslint-plugin-jsx-a11y
+     > eslint-config-airbnb 开启(react-hooks)使用 需要在 extends 中 add `['airbnb','airbnb/hooks']`
+2. `@typescript-eslint/eslint-plugin` 使用
+   package:
+
+   - @typescript-eslint/parser
+   - @typescript-eslint/eslint-plugin
+     > tips:
+     > It is important that you use the same version number for @typescript-eslint/parser and @typescript-eslint/eslint-plugin
+     > use:
+
+   ```javaScript
+   {
+     "parser": "@typescript-eslint/parser",
+     "plugins": ["@typescript-eslint"],
+     "rules": {
+       "@typescript-eslint/rule-name": "error"
+     }
+   }
+   #or
+   {
+     "extends": ["plugin:@typescript-eslint/recommended"]
+   }
+   ```
+
+3. `eslint-plugin-promise` 使用 Promise 语法写成最佳实践
+   package:
+
+   - npm install eslint-plugin-promise --save-dev
+     use:
+
+   ```javaScript
+   # 使用推荐的语法
+   {
+     "extends": ["plugin:promise/recommended"]
+   }
+   ```
+
+4. `eslint-plugin-unicorn`使用 提供了更多有用的配置项，比如我会用来规范关于文件命名的方式
+   package:
+   - npm install --save-dev eslint eslint-plugin-unicorn
+     use:
+   ```javaScript
+   # 使用推荐的语法
+   {
+     "extends": ["plugin:promise/recommended"]
+   }
+   ```
+5. `eslint-config-prettier` 使用 插件会禁用所有和 prettier 起冲突的规则
+   package:
+   - npm install eslint-config-prettier -D
+
+### stylelint
+
+> npm install stylelint stylelint-config-standard -D
+
+1. 然后在项目根目录新建 .stylelintrc.js 文件，输入以下内容
+   ```javaScript
+   # glob模式
+   module.exports = {
+      extends: ['stylelint-config-standard'],
+      rules: {
+       'comment-empty-line-before': null,
+       'declaration-empty-line-before': null,
+       'function-name-case': 'lower',
+       'no-descending-specificity': null,
+       'no-invalid-double-slash-comments': null,
+       'rule-empty-line-before': 'always',
+      },
+      ignoreFiles: ['node_modules/**/*', 'build/**/*']
+   }
+   ```
+2. 优秀的 plugins
+
+   ```shell
+   npm install stylelint-order stylelint-config-rational-order stylelint-declaration-block-no-ignored-properties -D
+
+   ```
+
+### tool
+
+1. `.gitignore` 输入 Add gitignore 命令 生成 .gitignore 文件
+2. `.npmrc` 终端 `touch .npmrc` 命令 在该文件内输入配置 `registry=https://registry.npm.taobao.org/`
+
+### EditorConfig 统一编辑器风格
+
+1. vscode 安装相应的扩展 EditorConfig For vs Code
+2. vscode 中使用快捷键 ctrl+shift+p 打开命令台，输入 Generate .editorcofig 即可快速生成 .editorconfig 文件
